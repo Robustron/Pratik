53beb27eda4394ecf62119ece46c06272a998313
Robustron <robustron@users.noreply.github.com>
2026-06-01 17:05:24 +0530

    feat: add education-related mockup image for ARCHON-7

--- components/ExecutionProtocols.tsx
+++ components/ExecutionProtocols.tsx
@@ -17,7 +17,7 @@ const projects = [
     accentText: 'text-purple-300',
     liveLink: 'https://archon-7.vercel.app/',
     githubLink: 'https://github.com/Robustron/ARCHON-7',
-    mockup: '/archon_aesthetic.png',
+    mockup: '/archon_education.png',
   },
   {
     title: 'ConnectHub',
